1c81bedcbd7f4a81a56083bb4b070c52c7854e41
Roberto Espi <teratux@gmail.com>
2022-03-16 15:07:37 -0400

    Add form key to ok and not ok urls. Set CSFR bypass to both Ok and Error callbacks

--- Controller/Pay/Error.php
+++ Controller/Pay/Error.php
@@ -2,7 +2,25 @@
 
 namespace Payin7\Mage2Payin7\Controller\Pay;
 
-class Error extends \Magento\Framework\App\Action\Action {
+use Magento\Framework\App\CsrfAwareActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\Request\InvalidRequestException;
+
+class Error extends \Magento\Framework\App\Action\Action implements CsrfAwareActionInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validateForCsrf(RequestInterface $request): ?bool {
+        return true;
+    }
 
     public function execute() {
         return $this->_redirect('checkout');
--- Controller/Pay/Ok.php
+++ Controller/Pay/Ok.php
@@ -2,8 +2,12 @@
 
 namespace Payin7\Mage2Payin7\Controller\Pay;
 
-class Ok extends \Magento\Framework\App\Action\Action {
+use Magento\Framework\App\CsrfAwareActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\Request\InvalidRequestException;
 
+class Ok extends \Magento\Framework\App\Action\Action implements CsrfAwareActionInterface
+{
     protected $_pageFactory;
 
     public function __construct(
@@ -11,6 +15,20 @@ class Ok extends \Magento\Framework\App\Action\Action {
             \Magento\Framework\View\Result\PageFactory $pageFactory) {
         $this->_pageFactory = $pageFactory;
         parent::__construct($context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createCsrfValidationException(RequestInterface $request): ?InvalidRequestException {
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validateForCsrf(RequestInterface $request): ?bool {
+        return true;
     }
 
     public function execute() {
